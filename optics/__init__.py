4e0b1e4092e311cab5260992dfc7a58db133fff9
Xu Chen <102787433+xuchen8410@users.noreply.github.com>
2026-02-23 15:44:59 -0700

    pip install -e

--- optics/__init__.py
+++ optics/__init__.py
@@ -1,1 +1,3 @@
-
+from .pupil import circular_pupil, zernike_defocus, apply_phase
+from .propagation import fraunhofer_psf, otf_from_psf, mtf_radial
+from .metrics import strehl_ratio, normalize_energy
